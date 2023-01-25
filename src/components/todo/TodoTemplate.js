0b8ccdb9a6d73328ac5c07ebab1bca8b36e414ba
yeaygit <eulim9926@naver.com>
2023-01-25 15:17:20 +0900

    # todos app api 호출 완성

--- src/components/todo/TodoTemplate.js
+++ src/components/todo/TodoTemplate.js
@@ -12,7 +12,7 @@ const TodoTemplate = () => {
         {
             id:1,
             title:'아침 산책',
-            done:true
+            done:false
         },
         {
             id:2,
@@ -22,19 +22,19 @@ const TodoTemplate = () => {
         {
             id:3,
             title:'샌드위치 사 먹기',
-            done:false
+            done:true
         },
         {
             id:4,
             title:'리액트 공부하기',
-            done:false
+            done:true
         },
     ];
 
 
   return (
     <div className='todo-template'>
-        <TodoHeader />
+        <TodoHeader todoList={todos}/>
         <TodoMain todoList={todos}/>
         <TodoInput />
     </div>
